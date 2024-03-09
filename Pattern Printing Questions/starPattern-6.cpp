5aa815850d2266b837835ceae59c16c991ec0292
Aryan Mishra <mishraaryan9204@gmail.com>
2024-03-10 00:02:13 +0530

    star pattern - 6

---
+++ Pattern Printing Questions/starPattern-6.cpp
@@ -0,0 +1,32 @@
+/*
+output: 
+           * * * *
+             * * *
+               * *
+                 *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cout<<"Enter number of rows: ";
+    cin>>a;
+    
+    int i = 0;
+    while(i<a){
+        int j = 0;
+        int k = 0;
+        while(j<i){
+            cout<<"  ";
+            j++;
+        }
+        while(k<(a-i)){
+            cout<<"* ";
+            k++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
