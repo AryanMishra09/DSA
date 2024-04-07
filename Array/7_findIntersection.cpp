d2ffb158eadd8771dac6610751dfcf660a4dca95
Aryan Mishra <mishraaryan9204@gmail.com>
2024-04-07 16:26:53 +0530

    otimized code for finding duplicate

--- Array/7_findIntersection.cpp
+++ Array/7_findIntersection.cpp
@@ -11,6 +11,8 @@ ex: input:  A : [1,2,3,3]
 #include <iostream>
 using namespace std;
 
+/*
+//bruteforce method
 void findIntersection(int a[], int b[], int sizea, int sizeb){
     for(int i=0; i<sizea; i++){
         for(int j=0; j<sizeb; j++){
@@ -22,10 +24,34 @@ void findIntersection(int a[], int b[], int sizea, int sizeb){
         }
     }
 }
+*/
+
+//optimized method using the point that array is sorted in non-decreasing order:
+void optimized(int arr[], int brr[], int sizea, int sizeb){
+    int i = 0, j = 0;
+    while(i<sizea && j<sizeb){
+        if(arr[i]==brr[j]){
+            if(i == 0 || arr[i] != arr[i - 1]){  // Check if the current element is not a duplicate
+                cout << arr[i] << " ";
+            }
+            i++;
+            j++;
+        } else if(arr[i]<brr[j]){
+            i++;
+        } else{
+            j++;
+        }
+    }
+}
+
 
 int main(){
-    int a[5] = {5,8,6,2,2};
-    int b[3] = {5,2,9};
-    findIntersection(a,b,5,3);
+    int a[6] = {1,2,2,2,3,4};
+    int b[4] = {2,2,3,3};
+    // cout<<"Find intersection : ";
+    // findIntersection(a,b,6,4);
+    // cout<<endl;
+    cout<<"Find intersection (optimized): ";
+    optimized(a,b,6,4);
     return 0;
 }
