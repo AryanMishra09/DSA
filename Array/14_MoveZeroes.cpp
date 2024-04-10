a217b3450f0451d9cead77d588f8d7d04a48983e
Aryan Mishra <mishraaryan9204@gmail.com>
2024-04-10 19:12:46 +0530

    Optimized move zero solution

--- Array/14_MoveZeroes.cpp
+++ Array/14_MoveZeroes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//my code:
 void moveZeroes(int a[], int size){
     for(int i=0; i<size; i++){
         if(a[i]==0){
@@ -18,8 +19,19 @@ void moveZeroes(int a[], int size){
     }
 }
 
+//optimized solution: 
+void optimized(int a, int size){
+    int i=0;
+    for(int j=0; j<num; j++){
+        if(a[j]!=0){
+            swap(a[i], a[j]);
+            i++;
+        }
+    }
+}
+
 int main(){
-    int a[7] = {0,1,3,0,4,0,0};
+    int a[7] = {1,3,0,0,4,0,0};
     moveZeroes(a,7);
     return 0;
 }
