639b34bb22fb6abb70ed60f4f40486d0cd993e79
Aryan Mishra <mishraaryan9204@gmail.com>
2024-05-03 17:29:00 +0530

    optimized calculate power code

--- 7. Recursion/10_PowerCalculate.cpp
+++ 7. Recursion/10_PowerCalculate.cpp
@@ -11,8 +11,35 @@ int calPower(int n, int m){
     if(m==0){
         return 1;
     }
-    ///recursive call:
-    return n * calPower(n,m-1);
+    if(m==1){
+        return n;
+    }
+    int ans = calPower(n, m/2);
+    if(n)
+}
+
+
+//highly optimized code:
+double myPow(double x, long long n) {
+        //base case:
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        // Handling negative exponent
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        //recursive call:
+        double ans = myPow(x, n/2);
+        if(n&1){
+            return x * (ans * ans);
+        }else{
+            return ans * ans;
+        }
 }
 
 int main(){
